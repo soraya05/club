6b4de72f97f544483f00368b0ed9ec09214bbf01
soraya05 <soryavanthay@hotmail.com>
2019-11-22 11:11:38 -0300

    Mi aplicación Django Girls, primer commit

--- sistema/socios/views.py
+++ sistema/socios/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from socios.models import Cuota 
+from socios.models import Cuota
 
 # Create your views here.
 def cuota_al_dia (request, numero_socio):
 	cuota = Cuota.objects.filter(socio__numero=numero_socio).last()
+	#mes = Cuota.objects.filter(cuota.mes).last()
 	return render(request, 'cuota.html', {'ultima_cuota': cuota})
+	
